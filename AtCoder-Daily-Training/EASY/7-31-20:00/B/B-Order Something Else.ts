272ab91975c4d76943ad1aad52f91fb73c9224a8
Sho0226 <s1F102201616@iniad.org>
2024-08-01 01:09:28 +0900

    end

--- AtCoder-Daily-Training/EASY/7-31-20:00/B/B-Order Something Else.ts
+++ AtCoder-Daily-Training/EASY/7-31-20:00/B/B-Order Something Else.ts
@@ -1,4 +1,8 @@
 const orderSomethingElse = () => {
-    let listPrice ;
+    let listPrice_l = 1;
+    let listPrice_r = 10 ^ 5
+    let listPriceCheaper_l = 1;
+    let listPriceCheaper_r = 111111;
+    
 
 }
